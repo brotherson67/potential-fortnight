c3720aa53a9fa2a6d015a314f024ec9d4ed75a8c
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-06 16:43:36 -0700

    finished adding resolve values for mutations

--- GraphQL_Introduction/server/server.js
+++ GraphQL_Introduction/server/server.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const { authMiddleware } = require("./utils/auth");
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,12 +20,12 @@ const startServer = async () => {
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-startServer();
+startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once("open", () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
